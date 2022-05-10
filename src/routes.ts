90d409e9b083aec8ba71dae086aa82f90aa8f3d2
Arthur Marconcini <arthur.m.ramos@hotmail.com>
2022-05-10 20:28:59 -0300

    feat: adjusts 2.0

--- src/routes.ts
+++ src/routes.ts
@@ -16,19 +16,17 @@ routes.post('/feedbacks', async (req, res) => {
     const submitFeedbackUseCase = new SubmitFeedbackUseCase(
     prismaFeedbacksRepository,
     nodemailerMailAdapter
-  )
+    )
 
-  await submitFeedbackUseCase.execute({
-    type, 
-    comment, 
-    screenshot
-  })  
+    await submitFeedbackUseCase.execute({
+      type, 
+      comment, 
+      screenshot
+    })  
 
-  return res.status(201).send()
+    return res.status(201).send()
   } catch (error) {
-    
+    console.log(error)
     return res.status(500).send()
-  }
- 
-  
+  }  
 })
